b59ca546e5716539860aeb6a9d97214d9386a41c
cerebralspill <mikengoc@gmail.com>
2023-11-06 10:48:46 -0500

    started new unit tests

---
+++ test/test_engine.py
@@ -0,0 +1,7 @@
+import unittest
+from datetime import datetime
+
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+from car_factory import CarFactory
